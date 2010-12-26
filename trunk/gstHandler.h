c6410f545e3df5fa51bdf22e4916fbb0f615cc46
shashank.personal@gmail.com <shashank.personal@gmail.com@88581d8e-628d-1381-61a7-e73197a1cca4>
2010-12-26 06:12:31 +0000

    commting intial strcutre , right now the code uses qt and gstreamer , gstreamer to take web cam data as input and qt widget

---
+++ trunk/gstHandler.h
@@ -0,0 +1,45 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  gstHandler.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  Friday 24 December 2010 03:42:53  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Shashank singh:w
+ *        Company:  Awaaz.me
+ *
+ * =====================================================================================
+ */
+
+#ifndef GSTHANDLER_H
+#define GSTHANDLER_H
+#include <gst/gst.h>
+#include <gst/app/gstappsink.h>
+#include <QObject>
+#include <QDebug>
+
+class gstHandler  : public QObject
+{
+Q_OBJECT
+
+private:
+GMainLoop *loop;
+GstElement *pipeline;
+GstElement *src;
+GstElement *sink;
+
+public:
+  
+gstHandler(int,char**);
+~gstHandler();
+int constructPipeline();
+};
+
+#endif
+
+
---
+++ trunk/main.cpp
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  Friday 24 December 2010 03:20:27  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  shashank singh, 
+ *        Company:  Awaaz.me
+ *
+ * =====================================================================================
+ */
+
+#include <QApplication>
+#include "gstHandler.h"
+
+
+int main(int argc , char ** argv) 
+{
+QApplication app(argc,argv);
+gstHandler pipeline(argc,argv);
+pipeline.constructPipeline();
+return 0;
+}
+
